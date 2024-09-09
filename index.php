6aca51230c8485d71fb84c0a3f10852cba218ed2
OLANREWAJU Oladimeji Nathaniel <olanrewajuoladimeji5@gmail.com>
2024-09-09 13:20:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -139,6 +139,169 @@ echo "<br>";
 
 echo substr($characterMe, 8 ,2);
 
+// interger whole number
+// float or double
+// number string
+// Arithemetic Operation
+
+// Addition
+// Subtract
+// Multiply
+// Division
+// BODMAS rules
+
+
+$age = -12;
+$gpa = 4.5;
+$homeNumber = "13";
+
+echo '<br>';
+echo $age;
+echo '<br>';
+echo $gpa;
+echo '<br>';
+echo $homeNumber;
+
+echo '<br>';
+
+echo $age * $homeNumber;
+
+echo '<br>';
+
+echo '<br>';
+echo '<br>';
+
+echo 3*4 *(6-3);
+
+echo '<br>';
+
+echo 3*4 *6-3;
+
+echo '<br>';
+
+echo PHP_INT_MAX;
+
+echo '<br>';
+
+var_dump(9223372036854775808);
+
+echo '<br>';
+
+echo PHP_FLOAT_MAX;
+
+echo '<br>';
+
+var_dump(1.7976931348623E+309);
+
+// echo '<br>';
+
+// echo is_double();
+
+// echo '<br>';
+
+// echo is_int();
+
+echo '<br>';
+
+// casting
+
+echo (int)$homeNumber ;
+echo '<br>';
+var_dump((int)$homeNumber);
+
+echo '<br>';
+
+// Math
+echo "<hr>";
+echo pi();
+echo '<br>';
+echo min($age, $gpa, $homeNumber);
+echo '<br>';
+echo max($age, $gpa, $homeNumber);
+echo '<br>';
+echo abs($age) , "<br>";
+echo sqrt(64);
+echo '<br>';
+echo round($gpa);
+echo '<br>';
+echo rand(10, 100);
+echo '<br>';
+echo floor(6.9);
+echo '<br>';
+echo ceil(6.4);
+echo "<hr>";
+
+// constant value
+
+define("name", "Olanrewaju Oladimeji");
+const age = 9;
+define("greeting", "welcome to class");
+
+echo name;
+echo '<br>';
+echo age;
+echo '<br>';
+echo greeting;
+echo '<br>';
+
+echo __FILE__;
+
+echo '<br>';
+
+MyFunction();
+
+echo '<br>';
+
+function MyFunction(){
+    echo __FUNCTION__;
+}
+
+echo '<br>';
+
+// Comparison Operator
+
+// == Equal
+// === identical
+// != Not equal
+// !== Not identical
+// > greater than
+// < less than
+// <= less than or equal to
+// >= greater than or equal to
+
+// ++ increment by one
+// -- Decrement y one
+
+// logical operators
+// || or
+// && and
+
+// Conditional Assignment
+// ? :
+// ?? NULL safety
+
+const num1 = 4;
+const num2 = 3;
+
+if(num1 == num2){
+    echo num1 . "And" . num2 . "are the same";
+}else{
+    echo "number are not the same";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
